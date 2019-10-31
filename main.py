be0b3566f3f69c91b476117aaa7d369bfd5925b5
yasiupl <contact@yasiu.pl>
2019-10-31 22:26:29 +0100

    working prototype

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
+#!/bin/python3
+
 import click
 import requests
 
 __author__ = "hs3city"
 
+"""EAN Barcode of Club Mate"""
+mate_barcode = 4029764001807
+
 @click.group()
 def main():
-    """
-    Simple CLI for managing Club Mate and other things @ hs3city.
-    """
-    pass
+	"""
+	Simple CLI using EAN Barcodes for managing Club Mate and (soon) other things @ hs3city.
+	"""
+	pass
 
 @main.command()
 @click.argument('barcode', type=click.IntRange(1000000000000, 9999999999999))
-def add(query):
-    """Add the product to the database"""
+def add(barcode):
+	"""Add the product to the database"""
 
-    amount = click.prompt('Please enter amount', type=click.IntRange(0, 40), default=1)
-
-    if(barcode == 4029764001807):
-        url = 'https://mate.at.hs3.pl/add/{}'
-        response = requests.get(url.format(amount))
-        click.echo(response)
-
-@main.command()
-@click.argument('barcode')
-def get(barcode):
-    """Return the amount of the product in the database"""
-    if(barcode == 4029764001807):
-        url = 'https://mate.at.hs3.pl/'
-        response = requests.get(url)
-        click.echo(response)
+	amount = click.prompt('Please enter the amount', type=click.IntRange(0, 40), default=1)
+	
+	if(barcode == mate_barcode):
+		url = 'https://mate.at.hs3.pl/add/{}'.format(amount)
+		response = requests.get(url)
+		click.echo(response.text)
 
 @main.command()
-@click.argument('barcode')
+@click.argument('barcode', type=click.IntRange(1000000000000, 9999999999999))
 def withdraw(barcode):
-    """Withdraw one item of the product in the database"""
-    if(barcode == 4029764001807):
-        url = 'https://mate.at.hs3.pl/one'
-        response = requests.get(url)
-        click.echo(response)
+	"""Withdraw one item of the product in the database"""
 
+	if(barcode == mate_barcode):
+		url = 'https://mate.at.hs3.pl/one'
+		response = requests.get(url)
+		click.echo(response.text)
 
+@main.command()
+@click.argument('barcode', type=click.IntRange(1000000000000, 9999999999999))
+def get(barcode):
+	"""Return the information about the product in the database"""
+
+	if(barcode == mate_barcode):
+		url = 'https://mate.at.hs3.pl/'
+		response = requests.get(url)
+		click.echo(response.text)
 
 if __name__ == "__main__":
-    main()
+	main()
